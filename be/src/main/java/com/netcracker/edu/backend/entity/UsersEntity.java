69ed07c00a97dc5da2c5a50e7910308c08915f01
Mongo5nfynfy <friendchpok@mail.ru>
2019-11-07 23:34:28 +0300

    add entities

---
+++ be/src/main/java/com/netcracker/edu/backend/entity/UsersEntity.java
@@ -0,0 +1,167 @@
+package com.netcracker.edu.backend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", schema = "netcrecker", catalog = "")
+public class UsersEntity {
+    private int idUser;
+    private String login;
+    private String password;
+    private String name;
+    private String surname;
+    private String email;
+    private int idRole;
+    private String patronymic;
+    private String faculty;
+    private Integer groupNumber;
+    private String specialty;
+
+    @Id
+    @Column(name = "id_user")
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    @Basic
+    @Column(name = "login")
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "surname")
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "id_role")
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    @Basic
+    @Column(name = "patronymic")
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    @Basic
+    @Column(name = "faculty")
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    @Basic
+    @Column(name = "group_number")
+    public Integer getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(Integer groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    @Basic
+    @Column(name = "specialty")
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UsersEntity that = (UsersEntity) o;
+
+        if (idUser != that.idUser) return false;
+        if (idRole != that.idRole) return false;
+        if (login != null ? !login.equals(that.login) : that.login != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (surname != null ? !surname.equals(that.surname) : that.surname != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (patronymic != null ? !patronymic.equals(that.patronymic) : that.patronymic != null) return false;
+        if (faculty != null ? !faculty.equals(that.faculty) : that.faculty != null) return false;
+        if (groupNumber != null ? !groupNumber.equals(that.groupNumber) : that.groupNumber != null) return false;
+        if (specialty != null ? !specialty.equals(that.specialty) : that.specialty != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idUser;
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (surname != null ? surname.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + idRole;
+        result = 31 * result + (patronymic != null ? patronymic.hashCode() : 0);
+        result = 31 * result + (faculty != null ? faculty.hashCode() : 0);
+        result = 31 * result + (groupNumber != null ? groupNumber.hashCode() : 0);
+        result = 31 * result + (specialty != null ? specialty.hashCode() : 0);
+        return result;
+    }
+}
